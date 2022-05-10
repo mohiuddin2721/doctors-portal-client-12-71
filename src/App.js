a0f54059ab5bf556c4ed001b8119e21967dc769e
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-10 17:03:24 +0600

    login component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './page/About/About';
 import Home from './page/Home/Home';
+import Login from './page/Login/Login';
 import Navbar from './page/Shared/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
