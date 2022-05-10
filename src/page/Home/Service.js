2ab920d83f4f187228f609b92efbbc0a64db9af6
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-10 23:05:08 +0600

    services complete

--- src/page/Home/Service.js
+++ src/page/Home/Service.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Service = () => {
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <figure class="px-10 pt-10">
-                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
+        <div className="card lg:max-w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" className="rounded-xl" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Shoes!</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
             </div>
         </div>
