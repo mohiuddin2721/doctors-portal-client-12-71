a2e400d40c76b763b16cd3a6cfa21627aef9a6fc
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-11 23:53:31 +0600

    service added on available appoinment page

--- src/page/Appointment/AppointmentBanner.js
+++ src/page/Appointment/AppointmentBanner.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { format } from 'date-fns';
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({date, setDate}) => {
 
     return (
         <div className="hero min-h-screen">
@@ -17,7 +16,6 @@ const AppointmentBanner = () => {
                         selected={date}
                         onSelect={setDate}
                     />
-                    <p>You have selected: {format(date, 'pp')}</p>
                 </div>
             </div>
         </div>
