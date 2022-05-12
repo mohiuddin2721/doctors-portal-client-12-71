85d2da7edcd801363270e73c98384e2b7aaa4bc8
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-13 00:05:46 +0600

    firebase login system added on login page

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
-        <div>
-            <h2>This is Login page.</h2>
+        <div className='flex h-screen justify-center items-center '>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center font-bold text-3xl">Log In</h2>
+                    
+                    <div className="divider text-secondary">OR</div>
+
+                    <button 
+                    onClick={() => signInWithGoogle()}
+                    className="btn btn-outline"
+                    >Continue with google</button>
+                </div>
+            </div>
         </div>
     );
 };
