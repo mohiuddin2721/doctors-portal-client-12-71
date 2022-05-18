b633f7daef8c4f20526a8c1121c590dd085e3b22
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-18 13:10:16 +0600

    photo uploaded in imgbb

--- src/page/Dashboard/AddDoctor.js
+++ src/page/Dashboard/AddDoctor.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const AddDoctor = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const { data: services, isLoading } = useQuery('service', () => fetch('http://localhost:5000/service').then(res => res.json()))
+
+    const imageStorageKey = 'c016290e08f02d9e406bf3ce2e202309';
+
+    /**
+     * 3 ways to store images
+     * 1. Third party storage //Free open public storage is ok for practice project
+     * 2. Your own storage in your own server (file system)
+     * 3. Database: Mongodb
+     * 
+     * YUP: to validate file: Search: Yup file validation for react hook form
+     */
 
     const onSubmit = data => {
-        alert('data', data);
+        // console.log('data', data);
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(result => {
+            // console.log('imgbb', result);
+            if(result.success) {
+                const img = result.data.url;
+                const doctor = {
+                    name: data.name,
+                    email: data.email,
+                    specialty: data.specialty,
+                    img: img
+                }
+                // send to your database
+            }
+        })
+    }
 
+    if (isLoading) {
+        return <Loading></Loading>
     }
 
     return (
@@ -32,7 +71,6 @@ const AddDoctor = () => {
                         />
                         <label className="label">
                             {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-
                         </label>
                     </div>
 
@@ -65,23 +103,35 @@ const AddDoctor = () => {
                         <label className="label">
                             <span className="label-text">Specialty</span>
                         </label>
+                        <select {...register('specialty')} class="select input-bordered w-full max-w-xs">
+                            {
+                                services.map(service => <option
+                                    key={service._id}
+                                    value={service.name}
+                                >{service.name}</option>)
+                            }
+                        </select>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Your Name</span>
+                        </label>
                         <input
-                            type="text"
-                            placeholder="specialty"
+                            type="file"
                             className="input input-bordered w-full max-w-xs"
-                            {...register("specialty", {
+                            {...register("image", {
                                 required: {
                                     value: true,
-                                    message: 'Specialization is Required'
+                                    message: 'Image is required'
                                 }
                             })}
                         />
                         <label className="label">
-                            {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password?.message}</span>}
-                            {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password?.message}</span>}
-
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
                         </label>
                     </div>
+
                     <input className='btn w-full max-w-xs text-white' type="submit" value="Add" />
                 </form>
             </div>
