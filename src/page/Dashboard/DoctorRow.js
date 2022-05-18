21377e5024a43aeefa8784e117b69e4d16796cc7
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-18 17:29:05 +0600

    doctor info added on doctor row

---
+++ src/page/Dashboard/DoctorRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const DoctorRow = ({ doctor, index }) => {
+    const { name, specialty, img } = doctor;
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <th>
+                <div class="avatar">
+                    <div class="w-16 rounded-full">
+                        <img src={img} alt={name} />
+                    </div>
+                </div>
+            </th>
+            <td>{name}</td>
+            <td>{specialty}</td>
+            <td><button class="btn btn-xs btn-error">Delete</button></td>
+        </tr>
+    );
+};
+
+export default DoctorRow;
--- src/page/Dashboard/ManageDoctors.js
+++ src/page/Dashboard/ManageDoctors.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
-const manageDoctors = () => {
+const ManageDoctors = () => {
+    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            
+            <h2 className="text-2xl">Manage doctors {doctors?.length}</h2>
+            <div>
+                <div class="overflow-x-auto">
+                    <table class="table w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Avatar</th>
+                                <th>Name</th>
+                                <th>Specialty</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                                ></DoctorRow>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default manageDoctors;
+export default ManageDoctors;
